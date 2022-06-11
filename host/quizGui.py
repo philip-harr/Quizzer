5f98b792b17df4c8a8c929a374ba65c9abb0e593
Philip Harr <philip.harr@outlook.de>
2022-06-11 19:30:27 +0200

    stabilty improvements

--- host/quizGui.py
+++ host/quizGui.py
@@ -2,9 +2,12 @@ from pdb import main
 import string
 from time import sleep
 import tkinter as tk
-from tkinter import Place, simpledialog
+from tkinter import Place
 from turtle import color
+from click import prompt
+from more_itertools import last
 import serial
+import os
 import threading
 
 class Team:
@@ -18,19 +21,33 @@ class Team:
 
 class SerialHandler:
     def __init__(self) -> None:
-        self.ser = serial.Serial("/dev/ttyUSB0", 500000, timeout=1)
-        self.ser.read_all()
+        device = "/dev/ttyUSB0"
+        while True:
+            if os.path.exists(device):
+                self.ser = serial.Serial("/dev/ttyUSB0", 500000, timeout=1)
+                self.ser.read_all()
+                break
+            else:
+                print("Quizzer not connected!")
+                print("Connect and press Enter to try again!")
+                input()
         
-    
+    def getPinOnce(self):
+        self.ser.write(b'GP\n')
+        while self.ser.in_waiting == 0:
+            sleep(0.01)
+
+        pin = self.ser.read()
+        return pin
+                 
+
     def getPin(self) -> int:
         
         while True:
-            self.ser.write(b'GP\n')
-            if self.ser.in_waiting > 0:
-                pin = self.ser.read()
-                if pin != b'{':
-                    self.clearPin()                  
-                    break
+            pin = self.getPinOnce()
+            if pin != b'{':
+                self.clearPin()                
+                break
 
             sleep(0.2)
     
@@ -50,9 +67,39 @@ class SerialHandler:
     def cleanup(self):
         self.ser.close()
 
+def setPin(mc, name):
+    global teamList, buttonThread, abortButtonThread
+    mc.clearPin()
+    while True:
+        pin = int.from_bytes(mc.getPinOnce(), 'big')
+        if pin != 123:            
+            success=True
+            for team in teamList:
+                if team.pinNumber == pin:
+                    print("Pin already used!\n")
+                    print("Try again!\n")
+                    success = False
+                    break
+            if success:
+                break
+        else:
+            sleep(0.2)
+
+        if abortButtonThread:
+            return
+
+    for item in teamList:
+        if name == item.name:
+            item.pinNumber = pin
+            
+    printLabels()
+
 teamList = []
+buttonThread = None
+abortButtonThread = False
 def addteam(window, mc):
-    name = simpledialog.askstring(title="Team",
+    global buttonThread
+    name = tk.simpledialog.askstring(title="Team",
                                   prompt="What's your Team Name?:")
     if name != None:
         if len(name) >= 20:
@@ -61,34 +108,20 @@ def addteam(window, mc):
         teamList.append(Team(name, -100))
         printLabels()
         window.update()
-        setPin(mc, -1)
+        buttonThread = threading.Thread(target=setPin, args=(mc, name))
+        buttonThread.start()
 
-def setPin(mc, index):
-    global teamList
-    mc.clearPin()
-    while True:
-        pin = int.from_bytes(mc.getPin(), 'big')
-        success=True
-        for team in teamList:
-            if team.pinNumber == pin:
-                print("Pin already used!\n")
-                print("Try again!\n")
-                success = False
-                break
-        if success:
-            break
+# def resetPin(mc):
+#     number = tk.simpledialog.askinteger(title="Reset Pin!",
+#                                      prompt="Teamnumber?")
 
-    teamList[index].pinNumber = pin
-    printLabels()
-
-def resetPin(mc):
-    number = simpledialog.askinteger(title="Reset Pin!",
-                                     prompt="Teamnumber?")
-
-    if number > 0 and number <= len(teamList):
-        setPin(mc, number-1)
+#     if number > 0 and number <= len(teamList):
+#         print("Press new button")
+#         teamList[number-1].pinNumber = -100
+#         printLabels()
+#         setPin(mc, number-1)
     
-    printLabels()
+#     printLabels()
 
 teamLabels = []
 def printLabels():
@@ -104,69 +137,134 @@ def printLabels():
       else:
         text = team.name + ": " +  str(team.points)
 
-      label = tk.Label(window, text=text, font=("Roboto", 20))
+      label = tk.Label(window, text=text, font=("Roboto", 18))
       label.place(x=15, y=(i*40)+60)
       teamLabels.append(label)
       i += 1
 
 def removeTeam():
-    global teamlabels, teamList
-    number = simpledialog.askinteger(title="Remove Team",
+    global teamlabels, teamList, abortButtonThread
+    number = tk.simpledialog.askinteger(title="Remove Team",
                                      prompt="Put in number of team to remove")
-    if number > 0 and number <= len(teamList):
-        del teamList[number-1]
-        printLabels()
+    if number != None:
+        if number > 0 and number <= len(teamList):
+            if teamList[number-1].pinNumber == -100 and buttonThread != None:
+                abortButtonThread = True
+                buttonThread.join()
+                abortButtonThread = False
+                buttonThread = None
+
+            del teamList[number-1]
+            printLabels()
    
 def clearAll():
-    teamList = []
-    printLabels()
+    global teamList, buttonThread, abortButtonThread 
+    res = tk.messagebox.askyesno(title="Clear Everything", message="Are you sure?")
+    if res == True:
+        teamList = []
+        if buttonThread != None:
+            abortButtonThread = True
+            buttonThread.join()
+            abortButtonThread = False
+            buttonThread = None
 
-def adjustPoints():
-    team = simpledialog.askinteger(title="Adjust Points",
-                                    prompt="Put in number of team to adjust points")
-    
-    if team > 0 and team <= len(teamList):
-        points = simpledialog.askinteger(title="Adjust Points",
-                                        prompt="Put in new number of points")
-        teamList[team-1].points = points
+        printLabels()
+
+lastTeamAnswered = None
+def adjustPoints(adjbtn):
+    global lastTeamAnswered
+
+    if lastTeamAnswered == None:
+        team = tk.simpledialog.askinteger(title="Adjust Points",
+                                        prompt="Put in number of team to adjust points")
     else:
-        print("Team number not available")
+        i = 1
+        for item in teamList:
+            if item.name == lastTeamAnswered.name:
+                team = i
+                break
+            i += 1
+        lastTeamAnswered = None
+    
+    if team != None:
+        if team > 0 and team <= len(teamList):
+            teamName = teamList[team-1].name
+            points = tk.simpledialog.askinteger(title="Adjust Points",
+                                            prompt= "Add/Subtract points for team: " + teamName)
+            teamList[team-1].points += points
+        else:
+            print("Team number not available")
 
+    adjbtn.configure(bg="lightgreen")
     printLabels()
 
-def question(mc, btn, window):
-    mc.clearPin()
-    # btn.configure(bg="yellow", text="Waiting...")
-    btn.configure(text="Waiting...", bg="yellow")
-    window.update()
-    # window.update()
+abort = False
+def question(mc,window, btn_game, btn_adj):
+    global abort, lastTeamAnswered
+    mc.clearPin()  
     
     while True:
-        pin = int.from_bytes(mc.getPin(), 'big')
+        pin = int.from_bytes(mc.getPinOnce(), 'big')
+        if pin != b'{':
+            to_answer = None
+            for team in teamList:
+                if team.pinNumber == pin:
+                    to_answer = team 
+                    lastTeamAnswered = team
+                    break
 
-        to_answer = None
-        for team in teamList:
-            if team.pinNumber == pin:
-                to_answer = team 
+            if to_answer != None:
+                msg = "Team " + to_answer.name + " to answer the question!"
+                answer = tk.messagebox.showinfo(title="Answer", message=msg)
+                btn_game.configure(bg="lightgreen", text="Game On!")
+                btn_adj.configure(bg="yellow")
+                printLabels()
                 break
-
-        if to_answer != None:
-            msg = to_answer.name + ": What is the answer?"
-            
-            answer = tk.messagebox.askquestion(title="Answer", message=msg)
-            
-            if answer == "yes":
-                points = simpledialog.askinteger(title="Congrats!",
-                                            prompt="Number of pts:")
-                to_answer.points += points
-                break
+                            
+                # if answer == "yes":
+                #     points = tk.simpledialog.askinteger(title="Congrats!",
+                #                                 prompt="Number of pts:",
+                #                                 parent=window)
+                #     to_answer.points += points
+                #     break
+                # else:
+                #     points = tk.simpledialog.askinteger(title="Thats too bad!",
+                #                                 prompt="Number of pts to subtract:",
+                #                                 parent=window)
+                #     to_answer.points -= points
+                #     break
         else:
-            mc.clearPin()
+            sleep(0.1)
+
+        if abort:
+            abort = False
             break
 
-        mc.clearPin()
+    mc.clearPin()
+
+
+questionThread = None
+def handleQuestion(mc, btn, adjbtn, window):
+    global questionThread, abort, teamList
+    if len(teamList) == 0:
+        print("Not possible when no Teams")
+        return
+
+    if questionThread == None:
+        btn.configure(text="Abort...", bg="yellow")
+        questionThread = threading.Thread(target=question, args=(mc, window, btn, adjbtn))
+        questionThread.start()
+
+    else:
+        abort = True
+        questionThread.join()
+        questionThread = None
+        btn.configure(bg="lightgreen", text="Game On!")
+        abort = False
+
+
     printLabels()
-    btn.configure(bg="lightgreen", text="Game On!")
+
 
 if __name__ == "__main__":
 
@@ -174,7 +272,7 @@ if __name__ == "__main__":
 
 
     window = tk.Tk()
-    window.geometry("700x300")
+    window.geometry("700x310")
     window.title("Digitaltechnik Quiz")
 
     # window.
@@ -183,23 +281,23 @@ if __name__ == "__main__":
     (tk.Label(window, text="Teams:", font=("Roboto", 25))).place(x=15, y=15)
         
 
-    clearBtn = tk.Button(window, text="Clear All", font=("Roboto", 20), command= lambda: clearAll())
-    clearBtn.place(x=550, y=00, height=50, width=150)
+    clearBtn = tk.Button(window, text="Clear All", font=("Roboto", 18), command= lambda: clearAll())
+    clearBtn.place(x=530, y=10, height=50, width=160)
     
-    removeTeamBtn = tk.Button(window, text="Rem. Team", command= lambda: removeTeam(), font=("Roboto", 20))
-    removeTeamBtn.place(x=550, y=50, height=50, width=150)
+    removeTeamBtn = tk.Button(window, text="Rem. Team", command= lambda: removeTeam(), font=("Roboto", 18))
+    removeTeamBtn.place(x=530, y=70, height=50, width=160)
 
-    addTeamBtn = tk.Button(window, text="Add Team", command= lambda: addteam(window, mc), font=("Roboto", 20))
-    addTeamBtn.place(x=550, y=100, height=50, width=150)
+    addTeamBtn = tk.Button(window, text="Add Team", command= lambda: addteam(window, mc), font=("Roboto", 18))
+    addTeamBtn.place(x=530, y=130, height=50, width=160)
 
-    setTeamBtn = tk.Button(window, text="Reset Btn", command= lambda: resetPin(mc), font=("Roboto", 20))
-    setTeamBtn.place(x=550, y=150, height=50, width=150)
+    # setTeamBtn = tk.Button(window, text="Reset Btn", command= lambda: resetPin(mc), font=("Roboto", 18))
+    # setTeamBtn.place(x=530, y=190, height=50, width=160)
 
-    adjBtn = tk.Button(window, text="Adj Pts", font=("Roboto", 20), command= lambda: adjustPoints())
-    adjBtn.place(x=550, y=200, height=50, width=150)
+    adjBtn = tk.Button(window, bg="lightgreen", text="Adj Pts", font=("Roboto", 18), command= lambda: adjustPoints(adjBtn))
+    adjBtn.place(x=530, y=190, height=50, width=160)
     
-    gameOn = tk.Button(window, text="Game On!", command= lambda: question(mc, gameOn, window), font=("Roboto", 20), bg="lightgreen")
-    gameOn.place(x=550, y=250, height=50, width=150)
+    gameOn = tk.Button(window, text="Game On!", command= lambda: handleQuestion(mc, gameOn, adjBtn, window), font=("Roboto", 18), bg="lightgreen")
+    gameOn.place(x=530, y=250, height=50, width=160)
 
     window.mainloop()
     mc.cleanup()
